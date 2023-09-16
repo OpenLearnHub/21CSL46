2a9a14470b830184fa8b3527b757d72ae3d2872d
Farha kousar <farhakousar1601@gmail.com>
2023-09-16 18:14:47 +0530

    initial commit

---
+++ pgm2a.py
@@ -0,0 +1,14 @@
+def fn(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fn(n - 1) + fn(n - 2)
+
+
+num = int(input("Enter  a number :"))
+if num > 0:
+    print("fn(", num, ") = ", fn(num), sep="")
+else:
+    print("Error in input")
