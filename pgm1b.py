0cc2449222f729162c15e32c1bfed93c3093b8ba
Farha kousar <farhakousar1601@gmail.com>
2023-09-16 11:41:39 +0530

    Initial commit

---
+++ pgm1b.py
@@ -0,0 +1,9 @@
+val = int(input("Enter a value : "))
+str_val = str(val)
+if str_val == str_val[::-1]:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+for i in range(10):
+    if str_val.count(str(i)) > 0:
+        print(str(i),"appears", str_val.count(str(i)), "times");
